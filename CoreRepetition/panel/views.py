d9d92f263ed278573bd9c595a8aebddfd0bd6b73
Alschn <adam.lisichin@gmail.com>
2020-09-04 15:05:41 +0200

    Add basic panel UI and simple homepage design
    Added basic homepage navbar with links, added user panel interface (sidebar, navbar, content section).

--- CoreRepetition/panel/views.py
+++ CoreRepetition/panel/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+notes = [
+    {
+        'author': 'Adam',
+        'title': 'Things I learned today',
+        'content': 'First note content',
+        'date_posted': 'September 4, 2020'
+    },
+    {
+        'author': 'Adam',
+        'title': 'Interesting lesson',
+        'content': 'Second note content',
+        'date_posted': 'September 5, 2020'
+    }
+]
+
 
 def panel_main(request):
-    return render(request, 'panel/main.html')
+    context = {
+        'notes': notes,
+    }
+    return render(request, 'panel/main.html', context)
 
 
 def panel_courses(request):
     return render(request, 'panel/courses.html')
-    

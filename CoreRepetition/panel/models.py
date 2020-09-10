fbda2646eb62fe90106a3c425d18e7097c79f462
Alschn <adam.lisichin@gmail.com>
2020-09-10 15:13:03 +0200

    Fix important issue
    Fixed issue where profile would not be created upon user registration.

--- CoreRepetition/panel/models.py
+++ CoreRepetition/panel/models.py
@@ -4,23 +4,28 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
-class Comment(models.Model):
-    content = models.TextField(max_length=1000)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
 class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    comments = models.ManyToManyField(Comment)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('panel-note-detail', kwargs={'pk': self.pk})
+
+
+# class Comment(models.Model):
+#     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+#     note = models.ForeignKey(Note, on_delete=models.CASCADE)
+#     content = models.TextField(max_length=300)
+#     updated = models.DateTimeField(auto_now=True)
+#     created = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return str(self.pk)
 
 
 # class Course(models.Model):
--- CoreRepetition/users/signals.py
+++ CoreRepetition/users/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import Profile
+from CoreRepetition.users.models import Profile
 
 
 @receiver(post_save, sender=User)
@@ -9,7 +9,4 @@ def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
-
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
     instance.profile.save()
